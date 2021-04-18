97c08e87f5cec52c2cafd96e37c3d5fdcbeb595f
eanovschoolgit <82503976+eanovschoolgit@users.noreply.github.com>
2021-04-18 15:24:22 +0200

    Add files via upload

---
+++ Scripts/ScriptProcessors/eanovgate/Interface.js
@@ -0,0 +1,56 @@
+Content.makeFrontInterface(400, 400);
+
+
+
+const var Dynamics = Synth.getEffect("Dynamics");
+const var Knob1 = Content.getComponent("Knob1");
+const var THRESHOLD = Content.getComponent("THRESHOLD");
+
+
+
+
+
+
+const var t = Engine.createTimerObject();
+t.setTimerCallback(function()
+{
+	var v = Dynamics.getAttribute(Dynamics.GateReduction);
+	
+	v = Engine.getDecibelsForGainFactor(v);
+	
+	Knob1.setValue(v);
+});
+
+t.startTimer(30);
+
+
+
+
+
+inline function onTHRESHOLDControl(component, value)
+{
+	Dynamics.setAttribute(Dynamics.GateThreshold, -value);
+};
+
+Content.getComponent("THRESHOLD").setControlCallback(onTHRESHOLDControl);
+function onNoteOn()
+{
+	
+}
+ function onNoteOff()
+{
+	
+}
+ function onController()
+{
+	
+}
+ function onTimer()
+{
+	
+}
+ function onControl(number, value)
+{
+	
+}
+ 
